a87459100a252b4769be0e36e2fb1d84bbe61930
kimlongngin <nginkimlong@gmail.com>
2018-06-13 21:01:42 +0800

    Change style

--- document/views.py
+++ document/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, Http404
 from django.template import loader 
 from django.views import generic
-from django.views.generic.edit import CreateView, UpdateView, DeleteView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 							
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, redirect 
 from django.contrib.auth import authenticate, logout, login
